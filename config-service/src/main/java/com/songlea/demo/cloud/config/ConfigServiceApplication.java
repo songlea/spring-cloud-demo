19def9df5df901e68d4bc9e8f65943565d646b7b
SongLea <18715278703@163.com>
2018-08-08 16:06:10 +0800

    配置中心模块

---
+++ config-service/src/main/java/com/songlea/demo/cloud/config/ConfigServiceApplication.java
@@ -0,0 +1,22 @@
+package com.songlea.demo.cloud.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * Spring Boot Config Server端
+ *
+ * @author Song Lea
+ */
+@SpringBootApplication
+@EnableConfigServer
+@EnableEurekaClient
+public class ConfigServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServiceApplication.class, args);
+    }
+
+}
