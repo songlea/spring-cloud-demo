30016716f84f5d3685304f292dd6507ddd7cbc1e
SongLea <18715278703@163.com>
2018-08-08 17:15:01 +0800

    hystrix dashboard移除到监控模块中

--- gateway-service/src/main/java/com/songlea/demo/cloud/gateway/GatewayServiceApplication.java
+++ gateway-service/src/main/java/com/songlea/demo/cloud/gateway/GatewayServiceApplication.java
@@ -1,12 +1,9 @@
 package com.songlea.demo.cloud.gateway;
 
-import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.context.annotation.Bean;
 
 /**
  * 智能路由/服务网关中心
@@ -18,21 +15,8 @@ import org.springframework.context.annotation.Bean;
 @EnableCircuitBreaker
 public class GatewayServiceApplication {
 
-    private static final String HYSTRIX_STREAM_PATH = "/hystrix.stream";
-    private static final String HYSTRIX_SERVLET_NAME = "HystrixMetricsStreamServlet";
-
     public static void main(String[] args) {
         SpringApplication.run(GatewayServiceApplication.class, args);
     }
 
-    @Bean
-    // 注册,否则dashboard界面无法访问到
-    public ServletRegistrationBean getServlet() {
-        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
-        ServletRegistrationBean<HystrixMetricsStreamServlet> registrationBean = new ServletRegistrationBean<>(streamServlet);
-        registrationBean.setLoadOnStartup(999);
-        registrationBean.addUrlMappings(HYSTRIX_STREAM_PATH);
-        registrationBean.setName(HYSTRIX_SERVLET_NAME);
-        return registrationBean;
-    }
 }
